e066ee2360c98fa62d0f3b7f5eaa0c41f2b50c0d
Jakub Nawrocki <nawrocki.jakub@gmail.com>
2025-01-04 18:40:29 +0100

    upgrade shm.c

--- shm.c
+++ shm.c
@@ -14,16 +14,14 @@
 
 
 struct SharedMemory {
-    int P; // Maksymalna liczba pszczó³ w ulu
-    int N; // Maksymalna liczba pszczó³ w populacji
+    int P;
+    int N;
 };
 
 
 int create_shared_memory(const char* pathname, int proj_id, size_t size) {
-    printf("Tworzenie klucza IPC z %s i %d\n", pathname, proj_id);
     key_t key = ftok(pathname, proj_id);
     if (key == -1) {
-        perror("ftok failed");
         exit(EXIT_FAILURE);
     }
     printf("Generowany klucz: %d\n", key);
@@ -31,26 +29,20 @@ int create_shared_memory(const char* pathname, int proj_id, size_t size) {
     int shmid = shmget(key, size, IPC_CREAT | 0666);
     if (shmid == -1) {
         perror("shmget failed");
-        printf("Rozmiar segmentu: %lu\n", size);
-        printf("Klucz IPC: %d\n", key);
         exit(EXIT_FAILURE);
     }
 
     return shmid;
 }
 
-// Od³¹cza segment pamiêci wspó³dzielonej od procesu
 void detach_shared_memory(void* addr) {
     if (shmdt(addr) == -1) {
-        perror("shmdt failed");
         exit(EXIT_FAILURE);
     }
 }
 
-// Usuwa segment pamiêci wspó³dzielonej
 void destroy_shared_memory(int shmid) {
     if (shmctl(shmid, IPC_RMID, NULL) == -1) {
-        perror("shmctl failed");
         exit(EXIT_FAILURE);
     }
 }

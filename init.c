6c24a25918120fbb743762ef3e7cf841dd44ea5f
Jakub Nawrocki <nawrocki.jakub@gmail.com>
2025-01-04 18:51:02 +0100

    Dodanie bibliotek i szkielet programu

---
+++ init.c
@@ -0,0 +1,15 @@
+#include "shm.h"
+#include "sem.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/sem.h>
+
+int main()
+{
+
+
+
+
+	return 0;
+}
---
+++ pszczola.c
@@ -0,0 +1,18 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include "shm.h"
+#include "sem.h"
+
+int main()
+{
+
+
+
+
+	return 0;
+}

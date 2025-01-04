97be695c8999cc963d4e8c817c6a12875a5fa3f5
Jakub Nawrocki <nawrocki.jakub@gmail.com>
2025-01-04 18:46:59 +0100

    Aktualizacja shm.h

--- shm.h
+++ shm.h
@@ -1,15 +1,17 @@
+#ifndef SHM_H
+#define SHM_H
 
 #include <sys/types.h>
 
-
+struct SharedMemory {
+    int P; // Maksymalna liczba pszczó³ w ulu
+    int N; // Maksymalna liczba pszczó³ w populacji
+    int il;
+};
 
 int create_shared_memory(const char* pathname, int proj_id, size_t size);
-
-
 void* attach_shared_memory(int shmid);
-
-
 void detach_shared_memory(void* addr);
-
 void destroy_shared_memory(int shmid);
 
+#endif

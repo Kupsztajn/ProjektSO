fee2629ff234e12416014e2127849f92b33e963b
Jakub Nawrocki <nawrocki.jakub@gmail.com>
2025-01-05 21:53:50 +0100

    Aktualizacja init, rozszerzanie pamieci dzielonej

--- init.c
+++ init.c
@@ -5,23 +5,24 @@
 #include <stdlib.h>
 #include <sys/sem.h>
 
-#define SEM_ULE 2  
-#define SEM_POP 3  
-#define SEM_ENT1 0
+#define SEM_ULE 2 
+#define SEM_POP 3 
+#define SEM_ENT1 0 
 #define SEM_ENT2 1 
 #define SEM_KROL 4
 #define SEM_LOCK 5
 
-int main()
-{
-	int shmid = create_shared_memory("/tmp", 'A', sizeof(struct SharedMemory));
+int main() {
 
-	shm->P = 10; // Maksymalna liczba pszczó³ w ulu
-	shm->N = 20; // Maksymalna liczba pszczó³ w populacji
+    int shmid = create_shared_memory("/tmp", 'A', sizeof(struct SharedMemory));
 
-	printf("Segment pamiêci wspó³dzielonej utworzony: shmid = %d\n", shmid);
-	printf("Wartoœci w pamiêci wspó³dzielonej: P = %d, N = %d\n", shm->P, shm->N);
 
+    struct SharedMemory* shm = (struct SharedMemory*)attach_shared_memory(shmid);
+
+    shm->P = 10; // Maksymalna liczba pszczó³ w ulu
+    shm->N = 20; // Maksymalna liczba pszczó³ w populacji
+    shm->nadmiar_ULE = 0;
+    shm->nadmiar_POP = 0;
     printf("Segment pamiêci wspó³dzielonej utworzony: shmid = %d\n", shmid);
     printf("Wartoœci w pamiêci wspó³dzielonej: P = %d, N = %d\n", shm->P, shm->N);
 
@@ -31,32 +32,24 @@ int main()
         exit(EXIT_FAILURE);
     }
 
-    
     int semid = semget(sem_key, 6, IPC_CREAT | 0666);
     if (semid == -1) {
         perror("semget failed");
         exit(EXIT_FAILURE);
     }
 
-    // Inicjalizacja wartoœci semaforów
-    inicjalizujSemafor(semid, SEM_ULE, shm->P);   
-    inicjalizujSemafor(semid, SEM_POP, shm->N);   
-    inicjalizujSemafor(semid, SEM_ENT1, 1); 
+    inicjalizujSemafor(semid, SEM_ULE, shm->P);
+    inicjalizujSemafor(semid, SEM_POP, shm->N);
+    inicjalizujSemafor(semid, SEM_ENT1, 1);
     inicjalizujSemafor(semid, SEM_ENT2, 1);
     inicjalizujSemafor(semid, SEM_KROL, 1);
     inicjalizujSemafor(semid, SEM_LOCK, 1);
 
-
-    
     while (1) sleep(1);
 
- 
     detach_shared_memory(shm);
-
 
     destroy_shared_memory(shmid);
 
     return 0;
-
-	return 0;
 }

aeca79352e9a38a5ed4e3ff694ba09abbf0c642d
Jakub Nawrocki <nawrocki.jakub@gmail.com>
2025-01-09 19:22:00 +0100

    dodanie funkcji inicjalizujacej do shm.h

--- shm.h
+++ shm.h
@@ -13,6 +13,8 @@ struct SharedMemory {
 
 int create_shared_memory(const char* pathname, int proj_id, size_t size);
 
+void zbior_sem_mem(int* shmid, struct SharedMemory** shm, int* semid);
+
 void* attach_shared_memory(int shmid);
 
 void detach_shared_memory(void* addr);

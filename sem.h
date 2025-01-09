9d9f0bfe9a68e427b320da3b1b8ae8493de14dc3
Jakub Nawrocki <nawrocki.jakub@gmail.com>
2025-01-09 21:42:20 +0100

    czyszcenie kodu sem h

--- sem.h
+++ sem.h
@@ -5,11 +5,15 @@
 #include <sys/ipc.h>
 #include <sys/sem.h>
 
-int alokujSemafor(key_t klucz, int number, int flagi);
-void inicjalizujSemafor(int semID, int number, int val);
-int zwolnijSemafor(int semID, int number);
-int waitSemafor(int semID, int number, int flags);
-void signalSemafor(int semID, int number);
-int valueSemafor(int semID, int number);
+//int alokujSemafor(key_t klucz, int number, int flagi);
+void init_semaphore(int semID, int number, int val);
+//int zwolnijSemafor(int semID, int number);
+//int waitSemafor(int semID, int number, int flags);
+//void signalSemafor(int semID, int number);
+//int valueSemafor(int semID, int number);
+int release_semaphore(int semid, int sem_num);
 
-#endif
+int acquire_semaphore(int semid, int sem_num);
+
+void release_entrance(int semid, int sem_entrance);
+#endif // SEM_H
